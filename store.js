8a0e970d4ad4365248a31f459e293dc55f029a55
Leon Marbukh <leonmarbukh@gmail.com>
2020-03-03 13:26:02 -0800

    added store.js

---
+++ store.js
@@ -0,0 +1,115 @@
+const STORE = {
+  questions: [//1
+        {
+        question: "Not adjusted for inflation, what is the highest-grossing film of all time in North America? 	",
+        options: [
+            "Star Wars: The Force Awakens", 
+            "Avatar", 
+            "Avengers: End Game", 
+            "Titanic"
+        ],
+        answer: "Avengers: End Game"
+        },
+        //2
+        {
+        question: "What Pixar film was the first computer-animated feature to be nominated for the Academy Award for Best Picture? ",
+        options: [
+            "Up",
+            "Toy Story", 
+            "Ratatouille",
+            "Inside Out"
+        ],
+        answer: "Up"
+        },
+        //3
+        {
+        question: "What is the highest-grossing romantic comedy of all time (not adjusted for inflation), earning $368.7 million at the box office on a budget of $5 million?",
+        options: [
+            "There’s Something About Mary", 
+            "What Women Want", 
+            "Hitch", 
+            "My Big Fat Greek Wedding"
+        ],
+        answer: "My Big Fat Greek Wedding"
+        },
+        //4
+        {
+        question: "In preparation for filming Lady Bird, director and writer Greta Gerwig took her cast and crew on a tour of what city, the film's main setting?", 
+        options: [
+            "Bakersfield",
+            "Santa Cruz",
+            "San Jose",
+            "Sacramento"
+            ],
+        answer: "Sacramento"
+        },
+        //5
+        {
+        question: "In 2010, the Academy of Motion Picture Arts and Sciences expanded the number of Best Picture nominees from 5 to 10 due to what popular 2008 film's notable snub",
+        options: [
+            "The Dark Knight", 
+            "Wall-E", 
+            "Slumdog Millionaire", 
+            "The Curious Case of Benjamin Button"
+        ],
+        answer: "The Dark Knight"
+        },
+        //6
+        {
+        question: "At the end of Love Actually, most main characters can be seen in what location, built in 1929 and located 14 miles (or 23 kilometres) west of Central London?",
+        options: [
+            "London Eye", 
+            "Heathrow Airport", 
+            "Tower Bridge", 
+            "Big Ben"
+        ],
+        answer: "Heathrow Airport"
+        },
+        //7
+        {
+        question: 'In the science fiction thriller Ex Machina, a computer programmer wins a contest to spend a week in an isolated mansion conducting what kind of test? This test is used to assess a machine\'s ability to behave indistinguishably from a human.',
+        options: [
+            "FizzBuzz", 
+            "ELIZA", 
+            "BASIC", 
+            "Turing"
+        ],
+        answer: "Turing"
+        },
+        //8
+        {
+        question: "Last revised on June 20, 2007 to include films released from 1997-2005, the American Film Institute's 100 Greatest American Films of All Time lists just one movie released in the 21st century. Name that film.", 
+        options: [
+            "Get Out", 
+            "Inception", 
+            "Lord of The Rings: The Fellowship of The Ring", 
+            "Toy Story"
+        ],
+        answer: "Lord of The Rings: The Fellowship of The Ring"
+        },
+        //9
+        {
+        question: "Better known for their rival roles on Game of Thrones, Emilia Clarke and Lena Headey have both portrayed the same iconic character, though one was on TV and the other was in a feature film. Who is that Character?",
+        options: [
+            "Buffy Anne Summers", 
+            "Princess Leia", 
+            "Katniss Everdeen", 
+            "Sarah Connor"
+        ], 
+        answer: "Sarah Connor"
+        },
+        //10
+        {
+        question: "Getting ready to watch a movie with kids? Before you take them to a screening of Marley and Me, you should probably check in with what website to avoid any unpleasant surprises...or future therapy costs?",
+        options: [
+            "rottentomatoes.com", 
+            "doesthedogdie.com", 
+            "movieviolence.com", 
+            "imdb.com"
+        ],
+        answer: "doesthedogdie.com"
+        }
+    ],
+    currentQuestion: 0,
+  score: 0
+};
